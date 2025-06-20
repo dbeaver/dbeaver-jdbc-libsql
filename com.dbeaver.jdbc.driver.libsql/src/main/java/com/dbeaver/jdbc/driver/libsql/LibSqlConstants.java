21476910b5be8e08f8d058a77bd20e96f8158aa5
Dmitrii Barnukov <dmitrii.barnukov@dbeaver.com>
2025-06-20 11:09:36 +0200

    dbeaver/dbeaver-jdbc-libsql#17 Added turso libsql url support

--- com.dbeaver.jdbc.driver.libsql/src/main/java/com/dbeaver/jdbc/driver/libsql/LibSqlConstants.java
+++ com.dbeaver.jdbc.driver.libsql/src/main/java/com/dbeaver/jdbc/driver/libsql/LibSqlConstants.java
@@ -20,8 +20,9 @@ import java.util.regex.Pattern;
 
 public class LibSqlConstants {
 
-    public static final Pattern CONNECTION_URL_EXAMPLE = Pattern.compile("jdbc:dbeaver:libsql:<server-url>");
-    public static final Pattern CONNECTION_URL_PATTERN = Pattern.compile("jdbc:dbeaver:libsql:(.+)");
+    public static final String CONNECTION_URL_EXAMPLES = "jdbc:dbeaver:libsql:<hostname>, libsql://<hostname>";
+    public static final String CONNECTION_PROTOCOLS_REGEXP = "jdbc:dbeaver:libsql:|libsql://";
+    public static final Pattern CONNECTION_URL_PATTERN = Pattern.compile("(" + CONNECTION_PROTOCOLS_REGEXP + ")(.+)");
 
     public static final int DRIVER_VERSION_MAJOR = 1;
     public static final int DRIVER_VERSION_MINOR = 0;
